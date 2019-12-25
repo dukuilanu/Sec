b36e489d25c9e2b41ff73fe26143450963359ef3
Dukuilanu <pytheaserrans@gmail.com>
2019-12-25 14:02:10 -0500

    add exception handling

--- sec.py
+++ sec.py
@@ -14,51 +14,58 @@ GPIO.setup(detectPin, GPIO.IN)
 GPIO.setwarnings(False)
 
 class interact(object):
-	def pull(self):
-		global enabled
-		response = urllib2.urlopen('http://192.168.1.143/api.php?sec=true&pulling=true')
-		html = response.read()
-		for line in html:
-			if line.rstrip() == "2":
+        def pull(self):
+                global enabled
+				try:
+                    response = urllib2.urlopen('http://192.168.1.143/api.php?sec=true&pulling=true')
+				except:
+					true
+                html = response.read()
+                for line in html:
+                        if line.rstrip() == "2":
                                 if enabled == 0:
                                         print "enabling motion sensor"
-				enabled = 1
-			if line.rstrip() == "3":
+                                enabled = 1
+                        if line.rstrip() == "3":
                                 if enabled == 1:
                                         print "disabling sensor"
-				enabled = 0
-			if line.rstrip() == "1":
-				return "picTime"
+                                enabled = 0
+                        if line.rstrip() == "1":
+                                return "picTime"
 
-	def event(self, state):
-		response = urllib2.urlopen('http://192.168.1.143/api.php?sec=true&event=' + state)
-
+        def event(self, state):
+		        try:
+                    response = urllib2.urlopen('http://192.168.1.143/api.php?sec=true&event=' + state)
+                except:
+					true
 interact = interact()
-
+first_time = time.time()
 while 1:
-	isPic = interact.pull()
-	if isPic == "picTime":
-		print "ispic"
+        isPic = interact.pull()
+        if isPic == "picTime":
+                print "ispic"
                 camera = PiCamera()
                 timeStamp = str(time.time()).replace( '.', '' )
-		camera.capture('/home/pi/www/photo/' + timeStamp + '.jpg')
+                camera.capture('/home/pi/www/photo/' + timeStamp + '.jpg')
                 response = urllib2.urlopen('http://192.168.1.143/api.php?sec=true&newPic=true&picTime=' + timeStamp)
                 camera.close()
-        
+
         if enabled == 1:
                 inputValue = GPIO.input(detectPin)
                 if inputValue == 1:
                         state = 1
                         camera = PiCamera()
-                        print "motion detected!"
+                        last_start_time = time.time()
+                        print "motion detected after: " + str(last_start_time - first_time)
+                        first_time = last_start_time
                         camera.start_recording('/home/pi/www/video/sec_' + str(time.time()) + '.h264', bitrate=4000000)
                         interact.event("started")
                         while state == 1:
                                 inputValue = GPIO.input(detectPin)
                                 if inputValue == 0:
                                          state = 0
-                        print "motion stopped."
+                        elapsed_time = time.time() - last_start_time
+                        print "motion stopped after " + str(elapsed_time)
                         camera.stop_recording()
                         camera.close()
                         interact.event("stopped")
-
